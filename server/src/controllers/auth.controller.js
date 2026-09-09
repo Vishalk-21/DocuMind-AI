3f95c00e4edd45c7257e4c7db0215a11392a9d9e
Vishal kumar <21230vishal@gmail.com>
2026-09-09 13:06:38 +0530

    Allow cross-site production auth cookies

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ const createAuthResponse = (user, res) => {
 
     res.cookie("documind_session", sessionValue, {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
